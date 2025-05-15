ada39ad76dcd45c4ccf748be57563daf79600b28
Zayyad Siddiqui <zayyad.siddiqui@kellogg.northwestern.edu>
2025-05-15 04:12:23 +0000

    initial creation of places page index, show, new, create

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,40 @@
+class PlacesController < ApplicationController
+
+  def index
+    # find all Places rows
+    @places = Place.all
+    # render places/index view
+  end
+
+  def show
+     # find a Place
+     @place = Place.find_by({"id" => params["id"]})
+     # p @company
+     @entries = Entry.where({"place_id" => @place["id"]})
+     # render places/show view with details about Place
+  
+  end
+
+  def new
+    # render view with new Place form
+  end
+
+  def create
+    # start with a new Place
+    @place = Place.new
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+
+    # save Place row
+    @place.save
+    # redirect user
+    redirect_to "/places"
+  end
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+  
+  resources "places"
+  # get("/places", {:controller => "places", :action => "index"})
+  # get("/places/new", {:controller => "places", :action => "new"})
+  # get("/places/:id", {:controller => "places", :action => "show"})
+  # post("/places", {:controller => "places", :action => "create"})
+  # get("/places/:id/edit", {:controller => "places", :action => "edit"})
+  # patch("/places/:id", {:controller => "places", :action => "update"})
+  # delete("/places/:id", {:controller => "places", :action => "destroy"})
+
+  resources "entries"
+  # get("/entries", {:controller => "entries", :action => "index"})
+  # get("/entries/new", {:controller => "entries", :action => "new"})
+  # get("/entries/:id", {:controller => "entries", :action => "show"})
+  # post("/entries", {:controller => "entries", :action => "create"})
+  # get("/entries/:id/edit", {:controller => "entries", :action => "edit"})
+  # patch("/entries/:id", {:controller => "entries", :action => "update"})
+  # delete("/entries/:id", {:controller => "entries", :action => "destroy"})
 end
